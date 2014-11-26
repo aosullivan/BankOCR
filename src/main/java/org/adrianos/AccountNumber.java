6878c84b9a9b45c6ca2da3111550e1b2cc76322a
Adrian Osullivan <adrian.osullivan@db.com>
2014-11-26 17:15:23 -0500

    Adding missing files

---
+++ src/main/java/org/adrianos/AccountNumber.java
@@ -0,0 +1,58 @@
+package org.adrianos;
+
+import java.util.function.IntUnaryOperator;
+import java.util.stream.IntStream;
+
+/**
+ * Represents a 9 character account number with validation logic
+ */
+public class AccountNumber {
+
+	private String accountNumber;
+
+	public AccountNumber(String accountNumber) {
+	    this.accountNumber = accountNumber;
+    }
+
+	/**
+	 * @return is valid if checksum divisible by 11 
+	 */
+	public boolean isValid() {
+	    return checkSum() % 11 == 0;
+    }
+
+	/**
+	 * @return sum of each numeric value in the account number multiplied by 
+	 * 9 - its position (with the string indexed 1 - 9) - see unit tests for example 
+	 */
+	protected int checkSum() {
+		IntUnaryOperator f = (pos) -> { return pos * Character.getNumericValue(accountNumber.charAt(9 - pos)); };
+		return IntStream.rangeClosed(1, 9)
+				.map(f)
+				.sum();
+    }
+	
+	private boolean isIllegible(){
+		return accountNumber.indexOf('?') != -1;
+	}
+
+	@Override
+    public int hashCode() {
+	    return accountNumber.hashCode();	    
+    }
+
+	@Override
+    public boolean equals(Object obj) {
+	    return accountNumber.equals(obj.toString());
+    }	
+	
+	@Override
+    public String toString() {
+		return accountNumber;
+	}
+
+	public String print() {
+	    return accountNumber + (isIllegible() ? " ILL" : (isValid() ? "": " ERR" ));
+    }	
+
+}
---
+++ src/test/java/org/adrianos/AccountNumberTest.java
@@ -0,0 +1,65 @@
+package org.adrianos;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AccountNumberTest {
+	
+	@Test
+    public void checksum() {
+		
+		AccountNumber sample = new AccountNumber("123456789");
+		int expected = (1 * 9) + (2 * 8) + (3 * 7) +
+					   (4 * 6) + (5 * 5) + (6 * 4) +
+					   (7 * 3) + (8 * 2) + (9 * 1);
+		assertEquals(expected, sample.checkSum());
+	}
+	
+    @Test
+    public void isValid() {
+    	
+    	AccountNumber valid1 = new AccountNumber("711111111");
+    	AccountNumber valid2 = new AccountNumber("123456789");
+    	AccountNumber valid3 = new AccountNumber("490867715");
+    
+    	assertTrue(valid1.isValid());
+    	assertTrue(valid2.isValid());
+    	assertTrue(valid3.isValid());
+    }
+
+    @Test
+    public void isInvalid() {
+    	
+    	AccountNumber invalid1 = new AccountNumber("888888888");
+    	AccountNumber invalid2 = new AccountNumber("490067715");
+    	AccountNumber invalid3 = new AccountNumber("012345678");
+    
+    	assertFalse(invalid1.isValid());
+    	assertFalse(invalid2.isValid());
+    	assertFalse(invalid3.isValid());
+    }
+
+    @Test
+    public void valueObject() {
+
+        AccountNumber eq1 = new AccountNumber("711111111");
+        AccountNumber eq2 = new AccountNumber("711111111");
+        AccountNumber notEq = new AccountNumber("711111112");
+        
+        assertEquals("Same numeric values are equal", eq1, eq2);
+        assertNotEquals("Different numeric values are unequal", eq1, notEq);
+        assertEquals("Same numeric values have same hashcode", eq1.hashCode(), eq2.hashCode());
+
+    }
+
+    @Test
+    public void stringValues() {
+        assertEquals("711111112", new AccountNumber("711111112").toString());
+        assertEquals("756373967", new AccountNumber("756373967").toString());
+    }
+
+}
